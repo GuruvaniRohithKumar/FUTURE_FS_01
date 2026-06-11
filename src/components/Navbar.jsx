52dc417afe9a7f5a7a2c6ff8ed2073f4a28aed2b
Guruvani Rohith Kumar <guruvanirohithkumar@gmail.com>
2026-06-11 23:10:41 +0530

    fix: updated navbar component with working profiles link text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
       <ul className="nav-links">
   <li><a href="#hero">Home</a></li>
   <li><a href="#about">About</a></li>
+  <li><a href="#profiles">Profiles</a></li>
   <li><a href="#projects">Projects</a></li>
   <li><a href="#achievements">Achievements</a></li>
   <li><a href="#education">Education</a></li>
